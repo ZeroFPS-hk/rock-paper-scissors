38df0fd34469171e57ad05c93d112d44c3e598db
ZeroFPS-hk <ctckong@connect.ust.hk>
2023-08-31 14:42:41 +0100

    Build initial js framework

---
+++ javascript.js
@@ -0,0 +1,18 @@
+const ROCK = "rock", PAPER = "paper", SCISSORS = "scissors";
+let playerChoice, computerChoice;
+
+function getComputerChoice(){
+
+}
+
+function playRound(){
+
+}
+
+function compareChoices(playerChoice, computerChoice){
+
+}
+
+function playGame(){
+    
+}
