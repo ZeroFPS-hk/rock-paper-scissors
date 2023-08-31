9a53906bc2028a3ab4e6617c0bf93b8421149588
ZeroFPS-hk <ctckong@connect.ust.hk>
2023-08-31 16:25:33 +0100

    Implement playGame function

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const ROCK = "rock", PAPER = "paper", SCISSORS = "scissors";
+let playerScore, computerScore;
 
 function getComputerChoice(){
     let i = Math.floor(Math.random()*3);
@@ -10,32 +11,39 @@ function getComputerChoice(){
 function playRound(){
     let playerChoice = prompt(`Enter "rock", "paper" or "scissors"!`).toLowerCase();
     let computerChoice = getComputerChoice();
-    compareChoices(playerChoice, computerChoice);
+    console.log(compareChoices(playerChoice, computerChoice));
 }
 
 function compareChoices(playerChoice, computerChoice){
     if(playerChoice!==ROCK && playerChoice!==PAPER && playerChoice!==SCISSORS){
-        console.log("Read the goddamn instructions.");
-        return -1;
+        computerScore++;
+        return "Read the goddamn instructions.";
     }
 
     if(playerChoice===computerChoice){
-        console.log(`You chose ${playerChoice}, computer chose ${computerChoice}. It's a tie!`);
-        return 0;
+        return `You chose ${playerChoice}, computer chose ${computerChoice}. It's a tie!`;
     }
 
     if((playerChoice===ROCK && computerChoice===SCISSORS) ||
     (playerChoice===PAPER && computerChoice===ROCK) ||
     (playerChoice===SCISSORS && computerChoice===PAPER)){
-        console.log(`You chose ${playerChoice}, computer chose ${computerChoice}. You win!`);
-        return 1;
+        playerScore++;
+        return `You chose ${playerChoice}, computer chose ${computerChoice}. You win!`;
     }
 
-    console.log(`You chose ${playerChoice}, computer chose ${computerChoice}. You lose!`);
-    return -1;
+    computerScore++;
+    return `You chose ${playerChoice}, computer chose ${computerChoice}. You lose!`;
 }
 
 function playGame(){
-    let round=0, playerScore=0, computerScore=0;
-    
+    playerScore=0, computerScore=0;
+    for (round=1; round<=5; round++){
+        playRound();
+        console.log(`Your score: ${playerScore}`);
+        console.log(`Computer score: ${computerScore}`);
+    }
+    let finalScore = `Final Score: ${playerScore}:${computerScore}. `;
+    playerScore>computerScore? console.log(finalScore + "You give the computer a much needed beating and let them know their place."):
+    computerScore>playerScore? console.log(finalScore + "The AI kicks your ass at rock paper scissors and takes over the world."):
+    console.log(finalScore + "You and your computer stare furiously at each other over the tie.");
 }
