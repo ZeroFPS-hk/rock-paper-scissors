e543e21bedb333aba023ba61368e937c42705c1b
ZeroFPS-hk <ctckong@connect.ust.hk>
2023-08-31 15:32:06 +0100

    Implent playRound and compareChoices

--- javascript.js
+++ javascript.js
@@ -9,11 +9,31 @@ function getComputerChoice(){
 }
 
 function playRound(){
-
+    playerChoice = prompt(`Enter "rock", "paper" or "scissors"!`).toLowerCase();
+    getComputerChoice();
+    compareChoices(playerChoice, computerChoice);
 }
 
 function compareChoices(playerChoice, computerChoice){
+    if(playerChoice!==ROCK && playerChoice!==PAPER && playerChoice!==SCISSORS){
+        console.log("Read the goddamn instructions.");
+        return -1;
+    }
 
+    if(playerChoice===computerChoice){
+        console.log(`You chose ${playerChoice}, computer chose ${computerChoice}. It's a tie!`);
+        return 0;
+    }
+
+    if((playerChoice===ROCK && computerChoice===SCISSORS) ||
+    (playerChoice===PAPER && computerChoice===ROCK) ||
+    (playerChoice===SCISSORS && computerChoice===PAPER)){
+        console.log(`You chose ${playerChoice}, computer chose ${computerChoice}. You win!`);
+        return 1;
+    }
+
+    console.log(`You chose ${playerChoice}, computer chose ${computerChoice}. You lose!`);
+    return -1;
 }
 
 function playGame(){
