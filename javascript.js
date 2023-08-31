faf04e48a5428b13d21c7699b44b931fe764a8ff
ZeroFPS-hk <ctckong@connect.ust.hk>
2023-08-31 15:00:04 +0100

    Implement getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -2,7 +2,10 @@ const ROCK = "rock", PAPER = "paper", SCISSORS = "scissors";
 let playerChoice, computerChoice;
 
 function getComputerChoice(){
-
+    let i = Math.floor(Math.random()*3);
+    i === 0? computerChoice = ROCK:
+    i === 1? computerChoice = PAPER:
+    computerChoice = SCISSORS;
 }
 
 function playRound(){
@@ -14,5 +17,5 @@ function compareChoices(playerChoice, computerChoice){
 }
 
 function playGame(){
-    
+
 }
